0b4b29ce935c302835b4e1c8723badad1a54a522
navinkumar-1 <gadadenavinkumar@gmail.com>
2026-09-21 17:13:20 +0530

    Add JavaScript practice

---
+++ 01-variables/questions.js
@@ -0,0 +1,85 @@
+// //variable 
+// a = 554254;
+// console.log(a);
+// b = 'navinkumar';
+// console.log(b)
+// c = 87425685268545n
+// console.log(c)
+// console.log(typeof(c))
+// console.log(typeof(a))
+// console.log("Type of variable :-",typeof(b))
+
+
+// //data types
+// //1
+// name = "Navinkumar arerao gadade";
+// console.log("String datatype  :-"+typeof(name))
+// //2
+// age = 20;
+// console.log("Number Datatype:-"+typeof(age))
+// //3
+// number = 41254126842641298n
+// console.log("Bigint datatype:- "+typeof(number))
+// //4
+// fine =true;
+// console.log("Boolean Datatype:- "+typeof(fine))
+// //5
+// let x;
+// console.log("Undefined Datatype:- "+typeof(x))
+// //6
+// let n = null;
+// console.log("Null Datatype :-",typeof(n))
+// //7
+// let sym1 = Symbol("identifier")
+// console.log("Symbol Datatype:- "+typeof(sym1))
+
+
+
+// //number in js 
+
+// let num1 = 5416854565;
+// console.log(typeof(num1))
+
+// let num2 = 54.16854565;
+// console.log(typeof(num2))
+
+// let num3 = -5416854565;
+// console.log(typeof(num3))
+
+// let num4 = -54.16854565;
+// console.log(typeof(num4))
+
+//opeartion in javascript programing 
+
+//1.Arithmetic Programig Language
+let num5 = 10;
+let num6 = 20;
+
+let result = num5+num6;
+let result1 = num5-num6;
+let result2 = num5*num6;
+let result3 = num5/num6;
+let result4 = num5%num6;
+
+console.log("Add:- "+result)
+console.log("Sub:- "+result1)
+console.log("MUl:- "+result2)
+console.log("Div:- "+result3)
+console.log("Mod:- "+result4)
+
+
+//NAN :- Not A Number
+
+let num1 = 0;
+let num2 = 0;
+let res = num1/num2;
+console.log("Result is : "+res+" :Type of Result is "+typeof(res))
+
+
+//opeartor prescendnce
+
+console.log(10*20+30-300);
+
+console.log(10**20+20-40/60*50)
+
+console.log(10+20-30/30*30)
