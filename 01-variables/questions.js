e095cf5228aea0c1c0ca3827173e30b69c6a0af9
navinkumar-1 <gadadenavinkumar@gmail.com>
2026-09-21 17:17:44 +0530

    Add JavaScript practice

--- 01-variables/questions.js
+++ 01-variables/questions.js
@@ -1,85 +1,99 @@
-// //variable 
-// a = 554254;
-// console.log(a);
-// b = 'navinkumar';
-// console.log(b)
-// c = 87425685268545n
-// console.log(c)
-// console.log(typeof(c))
-// console.log(typeof(a))
-// console.log("Type of variable :-",typeof(b))
+
+// 1. VARIABLES IN JAVASCRIPT
+
+let a = 554254;
+console.log("Value of a:", a);
+
+let b = "Navinkumar";
+console.log("Value of b:", b);
+
+let c = 87425685268545n;
+console.log("Value of c:", c);
+
+console.log("Type of a:", typeof a);
+console.log("Type of b:", typeof b);
+console.log("Type of c:", typeof c);
 
 
-// //data types
-// //1
-// name = "Navinkumar arerao gadade";
-// console.log("String datatype  :-"+typeof(name))
-// //2
-// age = 20;
-// console.log("Number Datatype:-"+typeof(age))
-// //3
-// number = 41254126842641298n
-// console.log("Bigint datatype:- "+typeof(number))
-// //4
-// fine =true;
-// console.log("Boolean Datatype:- "+typeof(fine))
-// //5
-// let x;
-// console.log("Undefined Datatype:- "+typeof(x))
-// //6
-// let n = null;
-// console.log("Null Datatype :-",typeof(n))
-// //7
-// let sym1 = Symbol("identifier")
-// console.log("Symbol Datatype:- "+typeof(sym1))
+// 2. DATA TYPES IN JAVASCRIPT
+
+// String
+let name = "Navinkumar Arerao Gadade";
+console.log("String:", typeof name);
+
+// Number
+let age = 20;
+console.log("Number:", typeof age);
+
+// BigInt
+let bigNumber = 41254126842641298n;
+console.log("BigInt:", typeof bigNumber);
+
+// Boolean
+let fine = true;
+console.log("Boolean:", typeof fine);
+
+// Undefined
+let x;
+console.log("Undefined:", typeof x);
+
+// Null
+let n = null;
+console.log("Null:", typeof n);
+
+// Symbol
+let sym1 = Symbol("identifier");
+console.log("Symbol:", typeof sym1);
 
 
+// 3. NUMBER DATA TYPE IN JAVASCRIPT
 
-// //number in js 
+let num1 = 5416854565;
+console.log("Positive Integer:", typeof num1);
 
-// let num1 = 5416854565;
-// console.log(typeof(num1))
+let num2 = 54.16854565;
+console.log("Positive Decimal:", typeof num2);
 
-// let num2 = 54.16854565;
-// console.log(typeof(num2))
+let num3 = -5416854565;
+console.log("Negative Integer:", typeof num3);
 
-// let num3 = -5416854565;
-// console.log(typeof(num3))
+let num4 = -54.16854565;
+console.log("Negative Decimal:", typeof num4);
 
-// let num4 = -54.16854565;
-// console.log(typeof(num4))
 
-//opeartion in javascript programing 
+// 4. ARITHMETIC OPERATORS
 
-//1.Arithmetic Programig Language
 let num5 = 10;
 let num6 = 20;
 
-let result = num5+num6;
-let result1 = num5-num6;
-let result2 = num5*num6;
-let result3 = num5/num6;
-let result4 = num5%num6;
+let addition = num5 + num6;
+let subtraction = num5 - num6;
+let multiplication = num5 * num6;
+let division = num5 / num6;
+let modulus = num5 % num6;
 
-console.log("Add:- "+result)
-console.log("Sub:- "+result1)
-console.log("MUl:- "+result2)
-console.log("Div:- "+result3)
-console.log("Mod:- "+result4)
-
-
-//NAN :- Not A Number
-
-let num1 = 0;
-let num2 = 0;
-let res = num1/num2;
-console.log("Result is : "+res+" :Type of Result is "+typeof(res))
+console.log("Addition:", addition);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Modulus:", modulus);
 
 
-//opeartor prescendnce
+// 5. NaN (NOT A NUMBER)
 
-console.log(10*20+30-300);
+let num7 = 0;
+let num8 = 0;
 
-console.log(10**20+20-40/60*50)
+let result = num7 / num8;
 
-console.log(10+20-30/30*30)
+console.log("Result:", result);
+console.log("Type of Result:", typeof result);
+
+
+// 6. OPERATOR PRECEDENCE (BODMAS)
+
+console.log(10 * 20 + 30 - 300);
+
+console.log(10 ** 20 + 20 - (40 / 60) * 50);
+
+console.log(10 + 20 - (30 / 30) * 30);
